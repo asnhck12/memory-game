3b466e137d78dbfb20705a87a7df700994f5b06d
AH LT <ahussain93@live.co.uk>
2024-08-14 16:37:00 +0100

    Updated formatting and added README

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# React + Vite
+# Memory Game
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+A memory game using cards showing 12 Pokemon characters, 12 random cards are generated for each round.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+The Memory Game App created using React.
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import MainLayout from './MainLayout'
+import ghLogo from './assets/github_icon.svg'
 
 function App() {
 
@@ -9,6 +10,18 @@ function App() {
       <h2>Memory Game</h2>
     </div>
     <MainLayout/>
+    <div id="footer">
+            <div id="gitHubText">
+                <a href="https://github.com/asnhck12" target="_blank">
+                    <p>asnhck12</p>
+                    </a>
+            </div>
+            <div id="gitHubIcon">
+                <a href="https://github.com/asnhck12" target="_blank">
+                    <img src={ghLogo} />
+                    </a>
+            </div>
+        </div>
     </>
   )
 }
